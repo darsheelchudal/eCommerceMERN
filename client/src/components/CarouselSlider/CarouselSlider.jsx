0b2457025ca82b2ca6ed0aff4ebef7a184ccd406
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-03 21:49:40 +0545

    carousel slider

---
+++ client/src/components/CarouselSlider/CarouselSlider.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+function CarouselSlider({ slides }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [slides.length]);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="relative my-4 overflow-hidden">
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {slides.map((slide, index) => (
+          <div key={index} className="flex-none w-full">
+            {slide}
+          </div>
+        ))}
+      </div>
+      <button
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+        onClick={goToPrevious}
+      >
+        <FaArrowLeft />
+      </button>
+      <button
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
+        onClick={goToNext}
+      >
+        <FaArrowRight />
+      </button>
+    </div>
+  );
+}
+
+export default CarouselSlider;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Categories from "../Layout/Categories";
+import Offer1 from "../../assets/offerBanner/offer1.jpg";
+import Offer2 from "../../assets/offerBanner/offer2.jpg";
+import Offer3 from "../../assets/offerBanner/offer3.jpg";
+import Offer5 from "../../assets/offerBanner/offer5.jpg";
+import Offer7 from "../../assets/offerBanner/offer7.jpg";
+import Offer8 from "../../assets/offerBanner/offer8.jpg";
+import CarouselSlider from "../CarouselSlider/CarouselSlider";
 
 function Home() {
+  const slides = [
+    <img src={Offer8} alt="Offer 8" className="h-80 w-full object-cover" />,
+    <img src={Offer1} alt="Offer 1" className="h-80 w-full object-cover" />,
+    <img src={Offer2} alt="Offer 2" className="h-80 w-full object-cover" />,
+    <img src={Offer3} alt="Offer 3" className="h-80 w-full object-cover" />,
+    <img src={Offer5} alt="Offer 5" className="h-80 w-full object-cover" />,
+    <img src={Offer7} alt="Offer 7" className="h-80 w-full object-cover" />,
+  ];
+
   return (
     <>
-      <div className="my-2 bg-slate-700 w-full h-max"></div>
       <Categories />
+      <div className="w-full flex justify-center">
+        <div className="w-[85%]">
+          <CarouselSlider slides={slides} />
+        </div>
+      </div>
     </>
   );
 }
