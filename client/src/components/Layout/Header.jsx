88c807d7f0e0a2a3043e3a1f144acc2c5c8c5fc2
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-04 20:53:23 +0545

    navbar form

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -9,21 +9,22 @@ function Header() {
     <>
       <div className="p-3 bg-blue-600 text-white">
         <div className="flex justify-around items-center">
-          <div>
+          <div className="flex gap-x-8">
             <Link to="/">
               <h1 className="text-2xl font-semibold">DMarket</h1>
             </Link>
+            <div className="relative">
+              <CiSearch className="sm:block hidden absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-800" />
+              <input
+                type="text"
+                name=""
+                id=""
+                className="sm:block hidden bg-slate-100 w-[600px] p-2 px-8 outline-none rounded-lg"
+                placeholder="Search for Products, Brands and More"
+              />
+            </div>
           </div>
-          <div className="relative">
-            <CiSearch className="sm:block hidden absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-800" />
-            <input
-              type="text"
-              name=""
-              id=""
-              className="sm:block hidden bg-slate-100 w-[350px] p-2 px-8 outline-none rounded-lg"
-              placeholder="Search for Products, Brands and More"
-            />
-          </div>
+
           <div className="flex gap-x-6 items-center">
             {" "}
             <Link to="/login">
