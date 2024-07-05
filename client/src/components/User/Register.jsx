4118bc0b98e41299a858cea79fc5e4ae88e164d4
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-05 11:57:57 +0545

    init

--- client/src/components/User/Register.jsx
+++ client/src/components/User/Register.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
+  const [previewSrc, setPreviewSrc] = useState(
+    "https://lh3.googleusercontent.com/a-/AFdZucpC_6WFBIfaAbPHBwGM9z8SxyM1oV4wB4Ngwp_UyQ=s96-c"
+  );
+
+  const loadFile = (event) => {
+    const input = event.target;
+    const file = input.files[0];
+
+    if (file) {
+      setPreviewSrc(URL.createObjectURL(file));
+    }
+  };
   return (
     <>
       <div className="flex justify-center items-center">
-        <div className="card flex h-[500px] bg-white my-8">
+        <div className="card flex h-[700px] bg-white my-8">
           <div className="poster w-[300px] bg-blue-500 text-white p-8 flex flex-col gap-y-4 overflow-hidden">
             <h1 className="font-bold text-xl">Looks like you're new here!</h1>
             <h2 className="font-semibold">
@@ -13,7 +26,42 @@ function Login() {
             </h2>
           </div>
           <div className="form p-9 w-[500px]">
-            <form action="" className="flex flex-col space-y-8">
+            <form
+              action=""
+              className="flex flex-col space-y-8"
+              encType="multipart/form-data"
+            >
+              <div className="shrink-0 mx-10">
+                <img
+                  id="preview_img"
+                  className="h-20 w-20 object-cover rounded-full"
+                  src={previewSrc}
+                  alt="Current profile photo"
+                />
+                <span>Choose profile picture</span>
+              </div>
+              <label className="block">
+                <span className="sr-only">Choose profile photo</span>
+                <input
+                  type="file"
+                  onChange={loadFile}
+                  className="block w-full text-sm text-slate-500
+              file:mr-4 file:py-2 file:px-4
+              file:rounded-full file:border-0
+              file:text-sm file:font-semibold
+              file:bg-violet-50 file:text-violet-700
+              hover:file:bg-violet-100
+            "
+                />
+              </label>
+              <input
+                type="text"
+                name=""
+                id=""
+                className="border-2 p-3 rounded-lg"
+                placeholder="Full Name *"
+              />
+
               <input
                 type="email"
                 name=""
@@ -21,12 +69,20 @@ function Login() {
                 className="border-2 p-3 rounded-lg"
                 placeholder="Email *"
               />
+
               <input
                 type="password"
-                name=""
-                id=""
+                name="cpassword"
+                id="cpassword"
                 className="border-2 p-3 rounded-lg"
                 placeholder="Password *"
+              />
+              <input
+                type="password"
+                name="cpassword"
+                id="cpassword"
+                className="border-2 p-3 rounded-lg"
+                placeholder="Confirm Password *"
               />
 
               <button className="flex justify-center bg-orange-500 text-white p-3 rounded-sm font-medium">
